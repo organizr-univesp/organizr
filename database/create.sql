09f86cd884a76764332d253b07896c0912606209
Lucas Viana <LSViana@users.noreply.github.com>
2021-10-23 01:18:39 -0300

    Atualização do script de criação do banco de dados

--- database/create.sql
+++ database/create.sql
@@ -1,35 +1,57 @@
-create table if not exists user_data(
-	login varchar(256)  unique not null,
-	senha varchar(256) not null,
-	nome varchar(256) not null,
-	sobrenome varchar(256),
-	idade int,
-	email varchar(256) unique not null,
-	id_usr SERIAL primary key 
-	)
-	
-create table if not exists produto(
-	nome varchar(20),
-	cor varchar(15),
-	categoria varchar(20),
-	id int primary key
-)
-
--- itens, sujeitos a alteração
-create table if not exists itens(
-	projeto varchar(256) not null,
-	id_projeto serial primary key,
-	id_usr int,
-	constraint fk_itemPessoa foreign key (id_usr) references user_data(id_usr)
-)
-
--- Formato postgreSQL padrão para inserção de datas é Ano/Mes/Dia
--- Etapas são referentes aos tipos de eventos criados pelo usuário.
-create table if not exists etapas(
-	dataInicio date,
-	dataFim date,
-	id_projeto int,
-	nomeEtapa varchar(50),
-	descEtapa text,
-	constraint fk_etapaItems foreign key (id_projeto) references items(id_projeto)
-)
+-- Dropping the tables
+drop table items_integrations;
+drop table integrations;
+drop table integration_categories;
+drop table items;
+drop table projects;
+drop table users;
+-- Creating the tables
+create table users (
+    id uuid not null primary key,
+    email varchar(256) not null unique,
+    password_hash varchar(1024) not null,
+    full_name varchar(256) null,
+    created_at timestamp without time zone not null default now(),
+    deleted_at timestamp without time zone null
+);
+create table projects (
+    id uuid not null primary key,
+    name varchar(256) not null,
+    color varchar(16) not null,
+    user_id uuid not null,
+    created_at timestamp without time zone not null default now(),
+    deleted_at timestamp without time zone null,
+    foreign key (user_id) references users (id)
+);
+create table items (
+    id uuid not null primary key,
+    name varchar(256) not null,
+    project_id uuid not null,
+    created_at timestamp without time zone not null default now(),
+    deleted_at timestamp without time zone null,
+    foreign key (project_id) references projects (id)
+);
+create table integration_categories (
+    id uuid not null primary key,
+    name varchar(256) not null,
+    color varchar(16) not null
+);
+create table integrations (
+    id uuid not null primary key,
+    name varchar(256) not null,
+    thumbnail_url varchar(512) not null,
+    slug varchar(64) not null unique,
+    integration_category_id uuid not null,
+    created_at timestamp without time zone not null default now(),
+    deleted_at timestamp without time zone null,
+    foreign key (integration_category_id) references integration_categories (id)
+);
+create table items_integrations (
+    item_id uuid not null,
+    integration_id uuid not null,
+    created_at timestamp without time zone not null default now(),
+    deleted_at timestamp without time zone null,
+    primary key (item_id, integration_id),
+    foreign key (item_id) references items (id),
+    foreign key (integration_id) references integrations (id)
+);
