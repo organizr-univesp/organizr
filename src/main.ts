f1c219f6dc2a2264b7dada581ebab1b9451f55ae
Lucas Viana <LSViana@users.noreply.github.com>
2021-10-23 13:38:00 +0000

    Hotfix: Formatação dos arquivos e remoção dos testes

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { AppModule } from '@/modules/app/index.module';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+    const app = await NestFactory.create(AppModule);
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- src/modules/app/index.module.ts
+++ src/modules/app/index.module.ts
@@ -3,8 +3,8 @@ import { AppController } from '@/modules/app/controllers/app/index.controller';
 import { AppService } from '@/modules/app/services/app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
